5913016a408efa5cd456ac082d1f256ac8a09292
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-09-04 23:33:03 -0400

    Change to Quart Part one - jinja2_markdown has an issue with Quart so out it goes. psycopg2 is sync, so it'll be replaced with asyncpg. I'll also probably take that opportunity to rewrite the storage system to be nicer even on WSGI servers with their threading.

--- app.py
+++ app.py
@@ -1,31 +1,30 @@
-import flask
-from flask import Flask
-from jinja2_markdown import MarkdownExtension
+import markdown
+import quart
+from quart import Quart
 
 import user_content
 from storage import JSONThingStorage
 
-app = Flask(__name__)
-app.jinja_env.add_extension(MarkdownExtension)
+app = Quart(__name__)
 post_storage = JSONThingStorage("interim-posts.json")
 comments_storage = JSONThingStorage("interim-comments.json")
 
 
 @app.route("/")
-def front_page():
+async def front_page():
     newest = sorted(post_storage.items(), key=lambda x: x[1]["timestamp"])[:10]
-    return flask.render_template("front_page.html", pageposts=newest)
+    return await quart.render_template("front_page.html", pageposts=newest)
 
 
 @app.route("/post/")
 @app.route("/post/<id_>")
-def get_post(id_: str = None):
+async def get_post(id_: str = None):
     if id_ is None:
-        return flask.redirect("/", code=302)
+        return quart.redirect("/")
     try:
         loaded = post_storage[id_]
     except KeyError:
-        return flask.Response(status=404)
+        return quart.Response("404", status=404)
     pos = user_content.Post.deserialize(loaded, id_)
     # load comments from storage
     comments = [
@@ -33,12 +32,13 @@ def get_post(id_: str = None):
         for k, v in comments_storage.items()
         if v["parent"] == pos.id
     ]
-    return flask.render_template(
+    return await quart.render_template(
         "post.html",
         title=pos.title,
         content=pos.content,
         comments=comments,
         children_of=comment_children,
+        md=markdown.markdown
     )
 
 
