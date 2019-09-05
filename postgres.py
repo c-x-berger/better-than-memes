e29271fe07d4d806bf2607419e7299712b0829a7
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-09-05 18:30:21 -0400

    get_comment func

--- postgres.py
+++ postgres.py
@@ -56,3 +56,8 @@ pool = asyncpg.create_pool(
 async def get_post(id_: str):
     async with pool.acquire() as con:
         return await con.fetchrow("SELECT * FROM posts WHERE id = $1", id_)
+
+
+async def get_comment(id_: str):
+    async with pool.acquire() as con:
+        return await con.fetchrow("SELECT * FROM comments WHERE id = $1", id_)
