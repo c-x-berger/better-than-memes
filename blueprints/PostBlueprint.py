66da3481cc51c6107f68aa16c266421092e6874c
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-09-06 20:49:15 -0400

    Add actual logic to post Blueprint

--- blueprints/PostBlueprint.py
+++ blueprints/PostBlueprint.py
@@ -1,8 +1,25 @@
-from flask import Blueprint
+from quart import Blueprint, g
+
+import postgres
 
 post_blueprint = Blueprint("post", __name__)
 
 
-@post_blueprint.route("/content")
-def contents():
-    return ""
+@post_blueprint.url_value_preprocessor
+async def preprocess(_, values):
+    g.id_ = values["post_id"]
+
+
+@post_blueprint.route("/")
+async def main_view():
+    id_ = g.id_
+    # load post
+    post = await postgres.get_post(id_)
+
+
+@post_blueprint.route("/api/content")
+async def contents():
+    conts = await postgres.pool.fetchrow(
+        "SELECT contents FROM posts WHERE id = $1", g.id_
+    )
+    return conts["contents"]
