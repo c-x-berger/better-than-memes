c3fb8a532734a45fba181872ed16143b7e70d512
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-09-14 00:09:24 -0400

    Starting login process

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from datetime import timezone
 
 import markdown
 import quart
+import quart.flask_patch
+from flask_login import LoginManager
 from quart import Quart
 
 import postgres
 from api_blueprint import api
+from login.user import User
 from post.post import post_blueprint
 
 app = Quart(__name__)
@@ -13,6 +16,8 @@ app.jinja_env.globals.update(md=markdown.markdown)
 app.jinja_env.globals.update(
     nix_time=lambda dt: dt.replace(tzinfo=timezone.utc).timestamp()
 )
+login_man = LoginManager()
+login_man.init_app(app)
 
 
 @app.route("/")
@@ -26,6 +31,17 @@ async def front_page():
 @app.before_serving
 async def init():
     await postgres.init_pool()
+
+
+@login_man.user_loader
+async def user_loader(username: str):
+    user = await postgres.pool.fetchrow(
+        "SELECT username FROM users WHERE username = $1", username
+    )
+    if user["username"]:
+        return User(user["username"])
+    else:
+        return None
 
 
 app.register_blueprint(post_blueprint, "/post/<post_id>")
