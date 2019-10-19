7c45d522548d2bcdc4ade6a26d8f09b0846376bc
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-10-19 01:38:41 -0400

    Correct formatting

--- blueprints/board/views.py
+++ blueprints/board/views.py
@@ -9,4 +9,6 @@ async def show_board(board: str):
     newest = await postgres.pool.fetch(
         "SELECT * FROM posts WHERE board <@ $1 ORDER BY timestamp DESC LIMIT 25", board
     )
-    return await quart.render_template("board/board.html", board=board, pageposts=newest)
+    return await quart.render_template(
+        "board/board.html", board=board, pageposts=newest
+    )
