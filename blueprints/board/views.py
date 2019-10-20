20f1f8e9c8952962dd170e973652883fd8f9cd7d
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-10-19 20:36:56 -0400

    Add board creation page

--- blueprints/board/views.py
+++ blueprints/board/views.py
@@ -1,3 +1,4 @@
+import flask_login
 import quart
 
 import postgres
@@ -12,3 +13,26 @@ async def show_board(board: str):
     return await quart.render_template(
         "board/board.html", board=board, pageposts=newest
     )
+
+
+@blue.route("/create", ["GET", "POST"])
+@flask_login.login_required
+async def create_board():
+    if quart.request.method == "GET":
+        return await quart.render_template("board/creation.html")
+
+    try:
+        path = (await quart.request.form)["path"]
+    except KeyError:
+        await quart.flash("no board given")
+        return await quart.render_template("board/creation.html")
+    viable_child = (
+        await postgres.pool.fetchrow("SELECT $1 <@ ANY(SELECT path FROM boards)", path)
+    )["?column?"]
+    if viable_child:
+        await postgres.pool.execute(
+            "INSERT INTO boards (path, creator) VALUES ($1, $2)",
+            path,
+            flask_login.current_user.id,
+        )
+        return quart.redirect(quart.url_for("board.show_board", board=path))
