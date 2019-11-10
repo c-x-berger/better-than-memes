271f9e8cbe85a2f67e9f5a1c365b013a69cad5e1
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-11-09 21:34:04 -0500

    Add setup.sql file

---
+++ setup.sql
@@ -0,0 +1,32 @@
+CREATE TABLE users
+(
+    username text PRIMARY KEY,
+    password text NOT NULL,
+);
+
+CREATE TABLE boards
+(
+    path    ltree PRIMARY KEY,
+    creator text REFERENCES users (username) NOT NULL
+);
+
+CREATE TABLE posts
+(
+    id        text PRIMARY KEY,
+    author    text REFERENCES users (username),
+    timestamp timestamp NOT NULL,
+    content   text,
+    title     text      NOT NULL,
+    board     ltree REFERENCES boards (path)
+);
+
+CREATE TABLE comments
+(
+    id        text PRIMARY KEY,
+    author    text      NOT NULL,
+    timestamp timestamp NOT NULL,
+    content   text      NOT NULL,
+    parent    text,
+    children  text[],
+    post      text REFERENCES posts (id)
+);
