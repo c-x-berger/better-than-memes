361d2074b14bb997d920c3a794735b2a6c20c9e3
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-11-15 16:16:58 -0500

    Create view for registration

--- blueprints/login/views.py
+++ blueprints/login/views.py
@@ -38,3 +38,37 @@ async def logout():
     else:
         flask_login.logout_user()
         return quart.redirect("/")
+
+
+@blue.route("create-account", ["GET", "POST"])
+async def create_user():
+    if request.method == "GET":
+        if not flask_login.current_user.is_anonymous:
+            return quart.redirect(quart.url_for("front_page"))
+        return await quart.render_template("login/create_account.html")
+
+    form = await request.form
+    username = form["username"]
+    pw = form["password"]
+    pw_verify = form["pass_verify"]
+    user_exists = (
+        await postgres.pool.fetchrow(
+            "SELECT exists(SELECT FROM users WHERE username = $1) as exists", username
+        )
+    )["exists"]
+    create_passed = not user_exists
+    if pw != pw_verify:
+        await quart.flash("passwords do not match")
+        create_passed = False
+    elif user_exists:
+        await quart.flash("username is already taken")
+    if not create_passed:
+        return await quart.render_template("login/create_account.html")
+    # create user, for real
+    await postgres.pool.execute(
+        "INSERT INTO users(username, password) VALUES ($1, $2)",
+        username,
+        flask_bcrypt.generate_password_hash(pw).decode("utf-8"),
+    )
+    flask_login.login_user(User(username))
+    return quart.redirect(quart.url_for("front_page"))
