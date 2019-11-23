0a545a6775abb9235f1b01f03546b350b9414f60
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-11-22 21:56:41 -0500

    Improve /board endpoint

--- blueprints/board/views.py
+++ blueprints/board/views.py
@@ -7,9 +7,26 @@ from . import blue
 
 @blue.route("/<board>")
 async def show_board(board: str):
-    newest = await postgres.pool.fetch(
-        "SELECT * FROM posts WHERE board <@ $1 ORDER BY timestamp DESC LIMIT 25", board
-    )
+    board_parts = [p for p in board.split(".") if p != ""]
+    sql_ready = []
+    for i in range(len(board_parts)):
+        part = board_parts[i]
+        if part == "*":
+            sql_ready.append("*{1}")
+        elif part == "**":
+            sql_ready.append("*")
+        elif part.startswith("_"):
+            excludes = part[1:].split(",")
+            sql_ready.append("!" + "|".join(excludes))
+        else:
+            # special cases that happen on last part
+            if i == len(board_parts) - 1:
+                if part != "-":
+                    sql_ready.extend((part, "*"))
+            else:
+                sql_ready.append(part)
+    query = ".".join(sql_ready)
+    newest = await postgres.pool.fetch("SELECT * FROM posts WHERE board ~ $1 ORDER BY timestamp DESC LIMIT 25", query)
     return await quart.render_template(
         "board/board.html", board=board, pageposts=newest
     )
@@ -18,6 +35,7 @@ async def show_board(board: str):
 @blue.route("/create", ["GET", "POST"])
 @flask_login.login_required
 async def create_board():
+    # TODO: prevent names from starting with control characters in show_board
     if quart.request.method == "GET":
         return await quart.render_template("board/creation.html")
 
