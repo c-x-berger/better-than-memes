4dc4a231894671799a5011596c0ea7e9551ae8c4
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2019-12-12 20:45:57 -0500

    Allow del tags, load strikeout extension

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import bleach
 
+from mdparse.extra_markdown import StrikeoutExtension
 from mdparse.images import ImageExtension
 
 ID_LENGTH = 7
@@ -11,13 +12,14 @@ POSTGRES_CONFIG = {
     "database": "bettermemes",
 }
 SECRET_KEY = "a very secret string"
-ALLOWED_HTML = (
-    bleach.ALLOWED_TAGS
-    + ["p", "pre", "br", "table", "thead", "tr", "th", "tbody", "td"]
-    + ["h{}".format(n) for n in range(1, 7)]
+ALLOWED_HTML = bleach.ALLOWED_TAGS
+ALLOWED_HTML.extend(
+    ["del", "p", "pre", "br", "table", "thead", "tr", "th", "tbody", "td"]
 )
+ALLOWED_HTML.extend(["h{}".format(n) for n in range(1, 7)])
 MARKDOWN_EXTENSIONS = [
     "markdown.extensions.fenced_code",
     "markdown.extensions.tables",
+    StrikeoutExtension(),
     ImageExtension(),
 ]
