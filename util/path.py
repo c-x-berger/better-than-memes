c1924bf2e6fc216e8478454e19547f660d32da82
Caleb Xavier Berger <caleb.x.berger@gmail.com>
2020-01-16 19:19:48 -0500

    Recude DB queries in getting comment children

--- util/path.py
+++ util/path.py
@@ -5,3 +5,7 @@ PATH_PART_REGEX = r"([\w\d_]+\.)+[\w\d_]+$"
 
 def is_valid(path: str) -> bool:
     return re.fullmatch(PATH_PART_REGEX, path) is not None
+
+
+def parent_of(path: str) -> str:
+    return ".".join(path.split(".")[:-1])
